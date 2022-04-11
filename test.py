4126accaad93994c9ce36b07696926aab026fe45
oruen321 <runyang.lian@hotmail.de>
2022-04-11 16:26:40 +0200

    change in main

--- test.py
+++ test.py
@@ -12,8 +12,9 @@ def mul(a,b):
     return a*b
 # added
 if __name__ == "__main__":
-    print("result is ", add(3, 4))
+    print("result is ", add(3, 9))
     print("result is ", sub(3, 9))
     print("result is ", mul(3, 4))
+    print("result is ", mul(3, 15))
     # add some some comments
     # test further results
