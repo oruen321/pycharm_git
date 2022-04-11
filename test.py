20353ebdf5ee97c3fa4d265b4d11de1513292c0f
oruen321 <runyang.lian@hotmail.de>
2022-04-11 08:39:11 +0200

    added function

--- test.py
+++ test.py
@@ -8,7 +8,10 @@ def add (a,b):
 def sub(a,b):
     return a-b
 
+def mul(a,b):
+    return a*b
 # added
 if __name__ == "__main__":
-    print("result is ", add(3,4))
+    print("result is ", add(3, 4))
     print("result is ", sub(3, 4))
+    print("result is ", mul(3, 4))
